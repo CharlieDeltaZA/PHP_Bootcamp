346331a19d99eef0dea003f208b93607d6641a11
Calvin Diogo <calvin@diogo.co.za>
2019-10-08 22:16:39 +0200

    Need to fix 1 test case : do_op_2

--- day01/ex11/do_op_2.php
+++ day01/ex11/do_op_2.php
@@ -1,15 +1,20 @@
 #!/usr/bin/php
 <?php
-	if (!$argc === 2)
+	if ($argc !== 2)
 	{
 		echo "Incorrect Parameters"."\n";
 		exit(1);
 	}
 
-	// $num1 = trim($argv[1]);
-	// $num2 = trim($argv[3]);
-	// $op = trim($argv[2]);
-	// $regex = /s*[0-9]+/s*[+-*/%]/s*[0-9]+/s*
+	$info = preg_match("/\s*([0-9]+)\s*([+,-,\*,\/,%])\s*([0-9]+)\s*/", $argv[1], $store);
+	var_dump($store);
+	// var_dump($info);
+	$num1 = trim($store[1]);
+	$num2 = trim($store[3]);
+	$op = trim($store[2]);
+
+// FIX ME
+// ./do_op_2.php  "six6*7sept"
 
 	if (!is_numeric($num1) || !is_numeric($num2))
 	{
