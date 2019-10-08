ae9c69cbce1bb0f2f5d6f28fe989bc6c1df8555f
Calvin Diogo <calvin@diogo.co.za>
2019-10-08 18:03:38 +0200

    Create do_op_2.php

---
+++ day01/ex11/do_op_2.php
@@ -0,0 +1,37 @@
+#!/usr/bin/php
+<?php
+	if (!$argc === 4)
+	{
+		echo "Incorrect Parameters"."\n";
+		exit(1);
+	}
+
+	$num1 = trim($argv[1]);
+	$num2 = trim($argv[3]);
+	$op = trim($argv[2]);
+	$regex = /s*[0-9]+/s*[+-*/%]/s*[0-9]+/s*
+
+	if (!is_numeric($num1) || !is_numeric($num2))
+	{
+		echo "Incorrect Parameters"."\n";
+		exit(1);
+	}
+
+	if ($op == '+')
+		$result = $num1 + $num2;
+	else if ($op == '-')
+		$result = $num1 - $num2;
+	else if ($op == '*')
+		$result = $num1 * $num2;
+	else if ($op == '/')
+		$result = $num1 / $num2;
+	else if ($op == '%')
+		$result = $num1 % $num2;
+	else
+	{
+		echo "Incorrect Parameters"."\n";
+		exit(1);
+	}
+	
+	echo "$result"."\n";
+?>
