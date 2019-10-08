56bd8c218bc448afe679f4f3e6a360850d21545b
Calvin Diogo <calvin@diogo.co.za>
2019-10-08 21:24:21 +0200

    Update do_op_2.php

--- day01/ex11/do_op_2.php
+++ day01/ex11/do_op_2.php
@@ -1,19 +1,19 @@
 #!/usr/bin/php
 <?php
-	if (!$argc === 4)
+	if (!$argc === 2)
 	{
 		echo "Incorrect Parameters"."\n";
 		exit(1);
 	}
 
-	$num1 = trim($argv[1]);
-	$num2 = trim($argv[3]);
-	$op = trim($argv[2]);
-	$regex = /s*[0-9]+/s*[+-*/%]/s*[0-9]+/s*
+	// $num1 = trim($argv[1]);
+	// $num2 = trim($argv[3]);
+	// $op = trim($argv[2]);
+	// $regex = /s*[0-9]+/s*[+-*/%]/s*[0-9]+/s*
 
 	if (!is_numeric($num1) || !is_numeric($num2))
 	{
-		echo "Incorrect Parameters"."\n";
+		echo "Syntax Error"."\n";
 		exit(1);
 	}
 
@@ -29,7 +29,7 @@
 		$result = $num1 % $num2;
 	else
 	{
-		echo "Incorrect Parameters"."\n";
+		echo "Syntax Error"."\n";
 		exit(1);
 	}
 	
