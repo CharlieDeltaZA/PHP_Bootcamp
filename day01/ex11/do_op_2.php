91a809d10c30fb6e50188c78c1bdbd750792eb37
Calvin Diogo <calvin@diogo.co.za>
2019-10-09 09:30:22 +0200

    do_op_2 seems good!

--- day01/ex11/do_op_2.php
+++ day01/ex11/do_op_2.php
@@ -6,14 +6,10 @@
 		exit(1);
 	}
 
-	preg_match("/\s*([0-9]+)\s*([+,-,\*,\/,%])\s*([0-9]+)\s*/", $argv[1], $store);
-	// var_dump($store);
+	preg_match("/\s*(.+)\s*([\+\-\*\/\%])\s*(.+)\s*/", $argv[1], $store);
 	$num1 = trim($store[1]);
 	$num2 = trim($store[3]);
 	$op = trim($store[2]);
-
-// FIX ME
-// ./do_op_2.php  "six6*7sept"
 
 	if (!is_numeric($num1) || !is_numeric($num2))
 	{
