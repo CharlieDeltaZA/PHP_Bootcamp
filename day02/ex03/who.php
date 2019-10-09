ec14e3c5fbd546dbe4a514b9e5c417a8897d7c01
Calvin Diogo <calvin@diogo.co.za>
2019-10-09 17:45:17 +0200

    wtf

---
+++ day02/ex03/who.php
@@ -0,0 +1,15 @@
+#!/usr/bin/php
+<?php 
+    $fd = fopen("/var/run/utmpx", "r");
+    $users = array();
+    while ($line = fread($fd, 628)) {
+        $line = unpack("a256user/a4id/a32line/ipid/itype/I2time", $line);
+        print_r($line);
+        if (strcmp($line['type'], "7") == 0)
+            array_push($users, $line);
+    }
+    date_default_timezone_set("Europe/Paris");
+    foreach($users as $user) {
+        printf("%s %s %s\n", $user['user'], $user['line'], date("M j H:i", $user['time1']));
+    }
+?>
