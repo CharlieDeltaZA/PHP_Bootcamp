59fb08b9aa015f62b5c6d705c86981202396a861
Calvin Diogo <calvin@diogo.co.za>
2019-10-10 13:55:07 +0200

    COOKIE MONSTER

---
+++ day03/ex03/cookie_crisp.php
@@ -0,0 +1,25 @@
+<?php
+	switch($_GET['action'])
+	{
+		case("get"):
+		{
+			if ($_GET['name'] && $_COOKIE[$_GET['name']] && !$_GET['value'])
+				echo $_COOKIE[$_GET['name']]."\n";
+			break;
+		}
+		case("set"):
+		{
+			if ($_GET['name'] && $_GET['value'])
+				setcookie($_GET['name'], $_GET['value']);
+			break;
+		}
+		case("del"):
+		{
+			if ($_GET['name'] && !$_GET['value'])
+				setcookie($_GET['name'], "", time() - 42);
+			break;
+		}
+		default:
+			break;
+	}
+?>
