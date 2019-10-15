c4b21acbc0dc9be401b1090b2077455a15c9bfa3
Calvin Diogo <calvin@diogo.co.za>
2019-10-15 18:27:29 +0200

    idk anymore

--- day05/ex11/ex11.sql
+++ day05/ex11/ex11.sql
@@ -1,6 +1,6 @@
-SELECT UPPER(`last_name`) AS `NAME`, `first_name`, `price`
-FROM `member`
-JOIN `subscription` ON member.id_sub = subscription.id_sub
-JOIN `user_card` ON member.id_member = user_card.id_user
-WHERE `price` > 42
-ORDER BY `last_name` ASC, `first_name` ASC;
+SELECT UPPER(last_name) AS 'NAME', first_name, subscription.price
+FROM user_card
+INNER JOIN `member` ON user_card.id_user = member.id_user_card
+INNER JOIN subscription ON member.id_sub = subscription.id_sub
+WHERE subscription.price > 42
+ORDER BY user_card.last_name, user_card.first_name;
