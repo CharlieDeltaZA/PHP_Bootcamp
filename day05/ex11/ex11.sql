269a948673429ca044999fa5767b209c05d7acb3
Calvin Diogo <calvin@diogo.co.za>
2019-10-15 17:44:22 +0200

    Update ex11.sql

--- day05/ex11/ex11.sql
+++ day05/ex11/ex11.sql
@@ -1,6 +1,6 @@
 SELECT UPPER(`last_name`) AS `NAME`, `first_name`, `price`
 FROM `member`
 JOIN `subscription` ON member.id_sub = subscription.id_sub
-JOIN `user_card` ON member.id_user_card = user_card.id_user
+JOIN `user_card` ON member.id_member = user_card.id_user
 WHERE `price` > 42
 ORDER BY `last_name` ASC, `first_name` ASC;
