1339a6828a23ef8af0ce31ff1c044ca9121fb143
Calvin Diogo <calvin@diogo.co.za>
2019-10-20 17:48:07 +0200

    d04/e00

---
+++ day04/ex00/index.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+    if ($_GET['login'] && $_GET['passwd'] && $_GET['submit'] === "OK")
+    {
+        $_SESSION['login'] = $_GET['login'];
+        $_SESSION['passwd'] = $_GET['passwd']; 
+    }
+?>
+
+<html>
+    <body>
+    	<form action="index.php" method="get">
+        	Username: <input type="text" name="login" value ="<?=$_SESSION['login']?>">
+			<br>
+        	Password: <input type="password" name="passwd" value ="<?=$_SESSION['passwd']?>">
+			<br>
+    	    <input type="submit" name="submit" value="OK">
+    	</form> 
+    </body>
+</html>
